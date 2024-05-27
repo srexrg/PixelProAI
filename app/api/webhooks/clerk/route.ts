e69f32c1ece150c64de9abe78e8b1463fa3a4a6b
srexrg <sreeragp276@gmail.com>
2024-05-27 12:49:38 +0530

    retry

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      userName: username!,
+      username: username!,
       firstName: first_name || "",
       lastName: last_name || "",
       photo: image_url,
@@ -83,7 +83,7 @@ export async function POST(req: Request) {
     const user = {
       firstName: first_name || "",
       lastName: last_name || "",
-      userName: username!,
+      username: username!,
       photo: image_url,
     };
 
